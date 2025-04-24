1d789f9999440b9914be1ec66888971bef013acd
clarapatta <clarapatta@gmail.com>
2025-04-24 13:01:44 -0300

    alteração da porta do server

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.get("/api/perguntas", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
