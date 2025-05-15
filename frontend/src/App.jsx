ab4ad6e308516518ddb7869ef14cb896b1be916e
Maria Clara <claramaria5751@gmail.com>
2025-05-15 12:37:27 -0300

    Criação da página de Perfil com css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-
 import Home from "./pages/Home";
-
 import Quiz from "./pages/Quiz";
-
 import Ranking from "./pages/Ranking";
-
+import PerfilScreen from "./pages/Perfil"; // ✅ CORRETA
 import PrivateRoute from "./pages/PrivateRoute";
 
 import "./App.css";
@@ -18,10 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={
-          <PrivateRoute> <Home /> </PrivateRoute>}/>
+          <PrivateRoute> <Home /> </PrivateRoute>} />
         <Route path="/quiz" element={
-            <PrivateRoute> <Quiz /> </PrivateRoute>}/>
+          <PrivateRoute> <Quiz /> </PrivateRoute>} />
         <Route path="/ranking" element={<Ranking />} />
+        <Route path="/perfil" element={
+          <PrivateRoute> <PerfilScreen /> </PrivateRoute>} /> {/* ✅ CORRIGIDA */}
       </Routes>
     </Router>
   );
